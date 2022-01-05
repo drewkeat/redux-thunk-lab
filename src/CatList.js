9b2f560f0ab9d67d305dbcafc9a0c27bbaaf38af
Drew Keat <thedrewkeat@gmail.com>
2022-01-04 22:14:53 -0700

    commit tree 804c04fc5b66308c9fb69ea9fff8d6a74fda5652

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,14 @@
-// write your CatList component here
+import React from 'react'
+
+export default function CatList(props) {
+  const renderCats = () => (
+    props.catPics.map(cat => {
+      return <img key={cat.id} src={cat.source_url} alt={`cat ${cat.id}`}></img>
+    })
+  )
+  return (
+    <div>
+      {renderCats()}
+    </div>
+  )
+}
